5de70d126763f0b63190c65237d2e08b38f1bbfe
bhakkkkktiiiiii <126847442+bhakkkkktiiiiii@users.noreply.github.com>
2023-04-11 20:00:19 +0000

    main

--- Icebreaker_Exercise_P_1.py
+++ Icebreaker_Exercise_P_1.py
@@ -1,1 +1,17 @@
-
+n=int(input())
+if n < 380 or n > 750:
+    print("Error")
+else:
+    
+    if n<450:
+        print('Violet')
+    elif n<495:
+        print('Blue')
+    elif n<570:
+        print('Green')
+    elif n<590:
+        print('Yellow')
+    elif n<620:
+        print('Orange')
+    else:
+        print('Red')
