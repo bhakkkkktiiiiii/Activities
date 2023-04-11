3656cb11f48abe952c4c5e74394ef9dd76c32879
bhakkkkktiiiiii <126847442+bhakkkkktiiiiii@users.noreply.github.com>
2023-04-11 22:12:02 +0000

    main

--- Exercise_Set_P_2_b.py
+++ Exercise_Set_P_2_b.py
@@ -1,1 +1,8 @@
-
+'''(b) How one can check if the restriction is on a number of words rather than letters?
+Write a function that allows a message with only 30 words.
+'''
+word=input()
+n=30
+word_list=word.split()[:n]
+output=" ".join(word_list)
+print(output)
