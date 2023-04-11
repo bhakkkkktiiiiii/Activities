25f08e7a2e5f5553ccab459f36efaaf01382ac0b
bhakkkkktiiiiii <126847442+bhakkkkktiiiiii@users.noreply.github.com>
2023-04-11 22:41:26 +0000

    main

--- Introductory_Exercises_Factor.py
+++ Introductory_Exercises_Factor.py
@@ -1,1 +1,4 @@
-
+T=float(input("Enter temperature in °C: "))
+w=float(input("Enter wind speed in Km/h: "))
+output= 13.12 + 0.6215*T - 11.37*(w*0.16) + 0.3965*T(w**0.16)
+print(round(output,2),"°C")
