2b4d78515437b655989176c46ccbeee46d3e1386
bhakkkkktiiiiii <126847442+bhakkkkktiiiiii@users.noreply.github.com>
2023-04-11 21:48:45 +0000

    main

--- Exercise_Set_P_1.py
+++ Exercise_Set_P_1.py
@@ -1,1 +1,10 @@
-
+def dis(prime,price):
+     
+    if prime:
+        discount_price=price*((1-0.08)*(1-0.15))
+    else:
+        discount_price=price*(1-0.08)
+    return discount_price
+prime=input()
+price=int(input())
+print(dis(prime,price))
--- Exercise_Set_P_2.py
+++ Exercise_Set_P_2.py
@@ -1,1 +1,19 @@
+'''(a) In few chit-chat programs, there is a limitation on the number of letters that you can send to your family and friends.
+
+Write a function that takes as input the,
+
+message and checks whether the number of letters is less than 200 or above. If the length of the information is less than 200, the chat should be returned.
+
+
+If the length of the chat is greater than 200, data consisting of only the first 200 characters should be returned.
+'''
+def limit_message(message):
+    if len(message) <= 200:
+        return message
+    else:
+        return message[0:200]
+message=input()
+print(limit_message(message))
+  
+
 
