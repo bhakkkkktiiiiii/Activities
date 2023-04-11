e8cd9dfe9860fc94ca806959bd6ffbe6dddf7394
bhakkkkktiiiiii <126847442+bhakkkkktiiiiii@users.noreply.github.com>
2023-04-11 22:45:17 +0000

    main

--- Introductory_Abstraction_Factor.py
+++ Introductory_Abstraction_Factor.py
@@ -1,1 +1,7 @@
 
+def wind_chill(temp_c, wind_sp):
+    change= 13.12 + 0.6215*temp_c - 11.37*(wind_sp**0.16) + 0.3965*temp_c*(wind_sp**0.16)
+    return change
+T=float(input())
+V=float(input())
+print(round(wind_chill(T,V),4),'°C')
