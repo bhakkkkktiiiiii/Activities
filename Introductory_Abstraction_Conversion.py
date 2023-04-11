a4477f374cde11ddbf137b8f41e35cc1a2a2df69
bhakkkkktiiiiii <126847442+bhakkkkktiiiiii@users.noreply.github.com>
2023-04-11 22:48:24 +0000

    main

--- Introductory_Abstraction_Conversion.py
+++ Introductory_Abstraction_Conversion.py
@@ -1,1 +1,8 @@
+def celsius_to_fahrenheit(celsius):
+    fahrenheit = (celsius * 9/5) + 32
+    return fahrenheit
+
+celsius = float(input("Enter temperature in Celsius: "))
+fahrenheit = celsius_to_fahrenheit(celsius)
+print("Temperature in Fahrenheit:", fahrenheit)
 
