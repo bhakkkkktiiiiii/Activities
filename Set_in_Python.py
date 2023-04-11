e42d312a16ac4c1f73cb5a49b1f763fa180d9574
bhakkkkktiiiiii <126847442+bhakkkkktiiiiii@users.noreply.github.com>
2023-04-11 22:03:35 +0000

    main

--- Set_in_Python.py
+++ Set_in_Python.py
@@ -1,1 +1,11 @@
+sentence = "Hey I am walking here I am walking here o captain my captain water water everywhere nor a drop to drink"
+print(sentence, "\n")
 
+sentence_list = list(map(str,sentence.split(' '))) 
+print(sentence_list , '\n')
+
+sentence_set=set(sentence_list)
+print(sentence_set, '\n')
+
+num=len(sentence_set)
+print(num,'\n')
