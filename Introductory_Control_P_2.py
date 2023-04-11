69072fa09f5f24543501818f6ebbb35097b11416
bhakkkkktiiiiii <126847442+bhakkkkktiiiiii@users.noreply.github.com>
2023-04-11 22:49:39 +0000

    main

--- Introductory_Control_P_2.py
+++ Introductory_Control_P_2.py
@@ -1,1 +1,20 @@
+num=int(input())
+def sum_of_divisors(num):          
+    div_sum = 0
+    for i in range(1, num):
+        if num % i == 0:
+            div_sum += i
+    return div_sum
 
+def category_check(num):            
+    div_sum = sum_of_divisors(num)
+    if div_sum > num:
+        return "abundant"
+    elif div_sum < num:
+        return "deficient"
+    else:
+        return "perfect"
+
+for num in range(1, num):
+    category = category_check(num)
+    print(f"{num}: {category}")
